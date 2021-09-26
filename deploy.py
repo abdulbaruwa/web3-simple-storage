3b8b87dad21129d2aa30d66bb5fba7910d762e6d
Shangoman <abdulbaruwa@hotmail.com>
2021-09-26 12:51:34 +0100

    Dump compiled sol to json file

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 from solcx import compile_standard, install_solc
+import json
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -20,4 +21,17 @@ compiled_sol = compile_standard(
     solc_version="0.6.0",
 )
 
-print(compiled_sol)
+# dump compiled sol in json file
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+
+# deplopy
+# get the bytecode from the compiled output
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+
+# print(bytecode)
+
+# get the ABI, from the compiled sol
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+print(abi)
